52a1b3862b3e334242a30dab23d2cfa508c03a03
Thainara Carvalho <thai.cds1@gmail.com>
2023-03-23 17:26:21 -0300

    subindo bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root :to => "hello#index"
+  # root :to => "hello#index"
+  root "dashboard#index"
 
   #      rota       metodo para acessar
   get '/people' => 'person#index'
